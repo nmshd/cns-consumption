519f11c99961db17d95e000aac335ebb8c7afb06
jkoenig <julian.koenig@js-soft.com>
2021-09-28 08:03:37 +0200

    chore: PR comments

--- .ci/writeBuildInformation.sh
+++ .ci/writeBuildInformation.sh
@@ -8,6 +8,6 @@ fi
 
 sed -i "s~{{dependencies}}~$(jq .dependencies package.json -cr)~" ./dist/ConsumptionBuildInformation.js
 sed -i "s/{{version}}/$(jq .version package.json -cr)/" ./dist/ConsumptionBuildInformation.js
-sed -i "s/{{build}}/$GITHUB_BUILD_NUMBER/" ./dist/ConsumptionBuildInformation.js
+sed -i "s/{{build}}/$BUILD_NUMBER/" ./dist/ConsumptionBuildInformation.js
 sed -i "s/{{commit}}/$(git rev-parse HEAD)/" ./dist/ConsumptionBuildInformation.js
 sed -i "s/{{date}}/$(date -u --iso-8601=seconds)/" ./dist/ConsumptionBuildInformation.js
