51151d5c9fe649d99f3f12b94f064d13530613ac
tnotheis <timo.notheisen@js-soft.com>
2022-04-22 11:56:14 +0200

    refactor: rename processor registry parameters

--- src/modules/requests/itemProcessors/RequestItemProcessorRegistry.ts
+++ src/modules/requests/itemProcessors/RequestItemProcessorRegistry.ts
@@ -7,24 +7,27 @@ type RequestItemConstructor = new () => RequestItem
 export class RequestItemProcessorRegistry {
     private readonly registry: Record<string, ProcessorConstructor | undefined> = {}
 
+    public registerProcessor(
+        processorConstructor: ProcessorConstructor,
+        itemConstructor: RequestItemConstructor
+    ): void {
+        if (this.registry.hasOwnProperty(itemConstructor.name)) {
+            throw new Error(
+                `There is already a processor registered for '${itemConstructor.name}''. Use 'replaceProcessorForType' if you want to replace it.`
+            )
+        }
+        this.registry[itemConstructor.name] = processorConstructor
+    }
+
+    public replaceProcessor(processorConstructor: ProcessorConstructor, itemConstructor: RequestItemConstructor): void {
+        this.registry[itemConstructor.name] = processorConstructor
+    }
+
     public getProcessorForItem(item: RequestItem): IRequestItemProcessor {
         const constructor = this.registry[item.constructor.name]
         if (!constructor) {
             throw new Error(`There was no processor registered for '${item.constructor.name}'.`)
         }
         return new constructor()
-    }
-
-    public registerProcessor(processor: ProcessorConstructor, requestItemType: RequestItemConstructor): void {
-        if (this.registry.hasOwnProperty(requestItemType.name)) {
-            throw new Error(
-                `There is already a processor registered for '${requestItemType.name}''. Use 'replaceProcessorForType' if you want to replace it.`
-            )
-        }
-        this.registry[requestItemType.name] = processor
-    }
-
-    public replaceProcessor(processor: ProcessorConstructor, forType: RequestItemConstructor): void {
-        this.registry[forType.name] = processor
     }
 }
