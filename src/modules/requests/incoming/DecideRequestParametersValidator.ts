77098bf0f366d0c985c81f2561eba55138d046ec
tnotheis <timo.notheisen@js-soft.com>
2022-04-28 10:31:09 +0200

    fix: use correct type for params of validator

--- src/modules/requests/incoming/DecideRequestParametersValidator.ts
+++ src/modules/requests/incoming/DecideRequestParametersValidator.ts
@@ -1,9 +1,9 @@
 import { ApplicationError, Result } from "@js-soft/ts-utils"
 import { ConsumptionRequest } from "../local/ConsumptionRequest"
-import { DecideRequestParametersJSON } from "./decide/DecideRequestParameters"
+import { InternalDecideRequestParametersJSON } from "./decide/InternalDecideRequestParameters"
 
 export class DecideRequestParametersValidator {
-    public validate(params: DecideRequestParametersJSON, request: ConsumptionRequest): Result<undefined> {
+    public validate(params: InternalDecideRequestParametersJSON, request: ConsumptionRequest): Result<undefined> {
         if (params.items.length !== request.content.items.length) {
             return Result.fail(
                 new ApplicationError("invalidNumberOfItems", "Number of items in Request and Response do not match")
