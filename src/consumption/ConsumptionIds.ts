2389438570208d55d8c9ae504397bf460f51fca3
jkoenig <julian.koenig@js-soft.com>
2022-05-11 13:39:04 +0200

    chore: prefix local ids with LCL

--- src/consumption/ConsumptionIds.ts
+++ src/consumption/ConsumptionIds.ts
@@ -1,8 +1,9 @@
 import { CoreIdHelper } from "@nmshd/transport"
 
 export class ConsumptionIds {
+    public static readonly draft = new CoreIdHelper("LCLDRF")
+    public static readonly setting = new CoreIdHelper("LCLSET")
+
     public static readonly attribute = new CoreIdHelper("ATT")
-    public static readonly draft = new CoreIdHelper("DRF")
     public static readonly request = new CoreIdHelper("REQ")
-    public static readonly setting = new CoreIdHelper("SET")
 }
